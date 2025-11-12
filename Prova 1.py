37c141f49b7f7a5963f35e1fb27f49995f82260f
iandres21219-cmyk <iandres21219@iesjoanramis.org>
2025-11-12 13:56:38 +0100

    ex2

--- Prova 1.py
+++ Prova 1.py
@@ -1,5 +1,9 @@
 A = float(input("Escriure el primer nombre: "))
 B = float(input("Escriure el segon nombre: "))
-C = float(input("Escriure el Tercer nombre: "))
-D = A +B 
-print("El resultat de la sume {} + {} és {}".format(A, B, D))
+D = float(input("Escriure el Tercer nombre: "))
+C = A + B + D
+print("El resultat de la suma {} + {} és {}".format(A, B, D, C))
+if C>20:
+    print("La suma és major que 20, que és {}".format(c))
+elif: num < 20
+    print("La suma és menor que 20, que és".format (c))
