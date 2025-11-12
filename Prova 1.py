8b67e14422fb3b1d24716d561a67031c8d395e83
iandres21219-cmyk <iandres21219@iesjoanramis.org>
2025-11-12 13:17:59 +0100

    primer ejercicio phyton

---
+++ Prova 1.py
@@ -0,0 +1,5 @@
+A = float(input("Escriure el primer nombre: "))
+B = float(input("Escriure el segon nombre: "))
+C = float(input("Escriure el Tercer nombre: "))
+D = A +B 
+print("El resultat de la sume {} + {} és {}".format(A, B, D))
