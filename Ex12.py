a23e5e3f561628008d0335a4619f0f000aadd2a2
iandres21219-cmyk <iandres21219@iesjoanramis.org>
2025-11-13 11:41:47 +0100

    Ex12

---
+++ Ex12.py
@@ -0,0 +1,8 @@
+Edat = int(input("introduce tu edat"))
+if Edat<18:
+ print ("Eres menor de edat")
+elif Edat>18:
+   print ("pots accidir ets mayor de edat")
+else:
+   print ("tens la edad justa")
+print ("Hola, tens {} años".format(Edat))
