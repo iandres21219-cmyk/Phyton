3a65052cb281a0499643bfeb440e4e2c082c0df3
iandres21219-cmyk <iandres21219@iesjoanramis.org>
2025-11-13 11:21:23 +0100

    Ex11

---
+++ Ex11.py
@@ -0,0 +1,3 @@
+nom = input ("como te llamas: ")
+cognom = input ("Cual es tu apellido: ")
+print("Hola, {} {}".format(nom, cognom))
--- Prova2.py
+++ Prova2.py
@@ -1,2 +1,1 @@
-for i in range (1,10,2):
-    print("{} * 5 = {}".format(i, i*5))
+print:(¿Como te llamas)
