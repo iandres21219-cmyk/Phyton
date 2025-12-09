ba138f6e4c3811ede7859d41919bee1df1edefe6
iandres21219-cmyk <iandres21219@iesjoanramis.org>
2025-12-09 13:39:47 +0100

    fins 78.py

--- Ex20.py
+++ Ex20.py
@@ -1,13 +1,3 @@
-"""
-Definir una funció invertir() que calculi la inversa d’una cadena. 
-Ex: invertir(“Soc del Ramis”) hauria de tornar la cadena “simaR led coS”.
-"""
-def invertir(s):
-    return s[::-1]
-
-#Programa principal
-cadena="Soc del Ramis"
-s = list(cadena)
-c = s.reverse()
-p = "".join(c)
-print("la inversa de {} és {}".format(cadena, invertir(cadena)))
+def invertir(cadena):
+    return ' '.join([paraula[::-1] for paraula in cadena.split()])
+print(invertir("Soc del Ramis"))
